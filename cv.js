e0787d5c7dbf547245aa5d0c9440b4914d70a1b6
YANINA MARTINEZ <yani.any25@gmail.com>
2023-01-30 09:32:04 -0300

    Add files via upload

---
+++ cv.js
@@ -0,0 +1,6 @@
+function btnChanger() {
+    let bg= document.getElementById("conteiner").style.background="lightblue";
+    let x= document.getElementById("btn-color").style.background="blue";
+    let y= document.getElementById("btn-color").style.color="gold";
+    
+}
